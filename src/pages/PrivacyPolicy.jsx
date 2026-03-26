f3fc4406e78d4b822205569d0012bbad242ac88a
Birxy12 <40819611+Birxy12@users.noreply.github.com>
2026-03-26 10:38:34 +0100

    Create PrivacyPolicy.jsx with privacy policy content

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,32 @@
+# Privacy Policy
+
+**Effective Date: 2026-03-26**
+
+This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from our website.
+
+## Information We Collect
+
+We collect information about you when you visit our website, including information about your device, web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the site, and information about how you interact with the site.
+
+## Use of Your Information
+
+We use the information we collect to:
+- Communicate with you
+- Screen our orders for potential risk
+- When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.
+
+## Sharing Your Information
+
+We share your Personal Information with third parties to help us use your Personal Information, as described above. We may also use your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
+
+## Your Rights
+
+If you are a European resident, you have the right to access the personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.
+
+## Contact Us
+
+For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at privacy@example.com or by mail using the details provided below.
+
+## Changes
+
+We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal, or regulatory reasons.
