747e26dc6dc73b0067f8e8cbc662c47571f81ac4
Birxy12 <40819611+Birxy12@users.noreply.github.com>
2026-03-26 10:40:06 +0100

    Update Footer.jsx to add proper links to privacy policy and terms of service.

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+// Footer.jsx
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="/privacy-policy">Privacy Policy</a></li>
+        <li><a href="/terms-of-service">Terms of Service</a></li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
